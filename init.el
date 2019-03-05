b1bfad4fd30b6b186ff863fc833c519475505b87
halo <halo@localhost.localdomain>
2019-03-04 23:29:08 -0500

    tune emacs

--- init.el
+++ init.el
@@ -1,5 +1,5 @@
 (when (member "M+ 1mn" (font-family-list))
-  (set-face-attribute 'default nil :font "M+ 1mn-15"))
+  (set-face-attribute 'default nil :font "M+ 1mn-12"))
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
@@ -15,13 +15,6 @@
   (package-install 'use-package))
 (require 'use-package)
 
-
-;; (use-package benchmark-init
-;;   :ensure t
-;;   :config
-;;   ;; To disable collection of benchmark data after init is done.
-;;   (add-hook 'after-init-hook 'benchmark-init/deactivate))
-
 ;; Vim mode
 (use-package evil
   :ensure t
@@ -33,7 +26,6 @@
   (setq evil-search-module 'evil-search)
   :config
   (evil-mode 1)
-  (hs-minor-mode 1)
   (modify-syntax-entry ?_ "w")
 ;;  (setcdr evil-insert-state-map nil)
 )
@@ -56,7 +48,7 @@
 (use-package doom-themes
   :ensure t
   :config
-  (load-theme 'doom-vibrant t))
+  (load-theme 'doom-one t))
 
 (use-package json-mode
   :ensure t)
@@ -73,11 +65,26 @@
   (highlight-indentation-mode 1)
 )
 
-;; NeoTree
-;(use-package neotree
-  ;:ensure t)
+(use-package hideshow
+  :hook ((prog-mode . hs-minor-mode)))
 
-;; (load-theme 'tango-dark)
+(use-package linum-relative
+  :ensure t
+  :init
+  (setq linum-relative-backend 'display-line-numbers-mode)
+  :config
+  (linum-relative-mode 1))
+
+;; (use-package projectile
+;;   :ensure t
+;;   :config
+;;   (define-key evil-normal-state-map (kbd "C-p") 'projectile-find-file)
+;;   (projectile-mode +1))
+
+(use-package fzf
+  :ensure t
+  :config
+  (define-key evil-normal-state-map (kbd "C-p") 'fzf))
 
 (setq truncate-lines 1)
 ;; Show matching paren
@@ -99,6 +106,7 @@ scroll-preserve-screen-position 1)
 (setq initial-scratch-message "")
 (setq inhibit-startup-message t)
 (xterm-mouse-mode 1)
+(global-hl-line-mode 1)
 
 (define-key evil-normal-state-map (kbd "C-h") 'windmove-left)
 (define-key evil-normal-state-map (kbd "C-l") 'windmove-right)
@@ -109,11 +117,15 @@ scroll-preserve-screen-position 1)
 (define-key evil-normal-state-map (kbd "SPC b b") 'buffer-menu)
 (define-key evil-normal-state-map (kbd "SPC SPC") 'evil-ex-nohighlight)
 (define-key evil-normal-state-map (kbd "RET") 'evil-toggle-fold)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("bf5bdab33a008333648512df0d2b9d9710bdfba12f6a768c7d2c438e1092b633" default)))
  '(package-selected-packages
    (quote
     (origami use-package powerline neotree json-mode highlight-indentation evil-numbers evil doom-themes auto-complete))))
